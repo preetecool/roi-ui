4969ac0a2e636d6f6b2d4deb1a6e832364aa927e
preetecool <preet@creal.ca>
2025-10-09 15:36:39 -0400

    adding keyboard nav for nav footer

--- registry/brook/ui/navigation-menu/navigation-menu.tsx
+++ registry/brook/ui/navigation-menu/navigation-menu.tsx
@@ -178,13 +178,14 @@ const NavigationMenuFooter = ({
           href={buttonHref}
           onClick={onButtonClick}
           className={styles.footerButton}
+          tabIndex={0}
         >
           {buttonText}
-          <NavigationMenuIcon>
+          <NavigationMenu.Icon className={cn(styles.chevron)}>
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M5 12h14M12 5l7 7-7 7"/>
             </svg>
-          </NavigationMenuIcon>
+          </NavigationMenu.Icon>
         </NavigationMenuLink>
       </div>
     );
