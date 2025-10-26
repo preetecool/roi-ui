081c6945087349d4dfbfbc82fbd682171e2bff9a
preetecool <preet@creal.ca>
2025-10-25 20:51:46 -0400

    dropdown menu motion improvements

--- registry/brook/examples/dropdown-menu-motion/dropdown-menu-motion.tsx
+++ registry/brook/examples/dropdown-menu-motion/dropdown-menu-motion.tsx
@@ -25,7 +25,7 @@ export default function DropdownMenuFramerMotion() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div ref={containerRef}>
+    <div ref={containerRef} style={{ position: "relative" }}>
       <MotionConfig
         transition={{
           type: "spring",
@@ -36,77 +36,64 @@ export default function DropdownMenuFramerMotion() {
         }}
       >
         <Menu.Root onOpenChange={setOpen} open={open}>
-          {!open && (
-            <Menu.Trigger
-              nativeButton
-              render={
-                <motion.button
-                  className={styles.button}
-                  layoutId="menu-wrapper"
-                />
-              }
-            >
-              <motion.div
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                initial={{ opacity: 0 }}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Ellipsis size={18} />
-              </motion.div>
-            </Menu.Trigger>
-          )}
+          <Menu.Trigger
+            render={
+              <motion.button
+                className={styles.button}
+                layoutId="menu-wrapper"
+              />
+            }
+          >
+            {!open && <Ellipsis size={18} />}
+          </Menu.Trigger>
 
           <AnimatePresence>
             {open && (
-              <Menu.Portal keepMounted key="portal">
+              <Menu.Portal container={containerRef} key="portal">
                 <Menu.Positioner
                   anchor={containerRef}
-                  className={styles.positioner}
+                  render={<motion.div className={styles.positioner} layout />}
                   side="top"
-                  sideOffset={8}
+                  sideOffset={20}
                 >
-                  <Menu.Popup
-                    render={
-                      <motion.div
-                        className={styles.popup}
-                        layoutId="menu-wrapper"
-                      />
-                    }
-                    style={{ transition: "none" }}
-                  >
-                    <ul>
-                      <div style={{ height: "4px", width: "100%" }} />
-                      {menuItems.map((item, index) => (
-                        <motion.li key={item.label}>
-                          {index === menuItems.length - 1 && (
-                            <div className={styles.seperatorWrapper}>
-                              <motion.div className={styles.separator} />
-                            </div>
-                          )}
-                          <Menu.Item
-                            className={
-                              item.label === "Logout"
-                                ? styles.deleteItem
-                                : undefined
-                            }
-                            nativeButton
-                            render={
-                              <motion.button className={styles.menuItem} />
-                            }
-                          >
-                            <item.icon size={14} />
-                            {item.label}
-                          </Menu.Item>
-                        </motion.li>
-                      ))}
-                      <div style={{ height: "4px", width: "100%" }} />
-                    </ul>
-                  </Menu.Popup>
+                  <motion.div layoutId="menu-wrapper">
+                    <Menu.Popup
+                      render={
+                        <motion.div
+                          className={styles.popup}
+                          style={{ borderRadius: "0.3rem" }}
+                        />
+                      }
+                    >
+                      <motion.ul>
+                        <div style={{ height: "4px", width: "100%" }} />
+                        {menuItems.map((item, index) => (
+                          <motion.li key={item.label}>
+                            {index === menuItems.length - 1 && (
+                              <div className={styles.seperatorWrapper}>
+                                <motion.div className={styles.separator} />
+                              </div>
+                            )}
+                            <Menu.Item
+                              className={
+                                item.label === "Logout"
+                                  ? styles.deleteItem
+                                  : undefined
+                              }
+                              nativeButton
+                              render={
+                                <motion.button className={styles.menuItem} />
+                              }
+                            >
+                              <item.icon size={14} />
+                              {item.label}
+                            </Menu.Item>
+                          </motion.li>
+                        ))}
+                        <div style={{ height: "4px", width: "100%" }} />
+                      </motion.ul>
+                    </Menu.Popup>
+                  </motion.div>
                 </Menu.Positioner>
               </Menu.Portal>
             )}
