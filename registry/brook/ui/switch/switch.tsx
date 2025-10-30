b421faa0251f9fd1ae14ace65fbbd5b8445cdb0d
preetecool <preet@creal.ca>
2025-10-30 16:44:44 -0400

    improving switch style

--- registry/brook/ui/switch/switch.tsx
+++ registry/brook/ui/switch/switch.tsx
@@ -4,18 +4,24 @@ import { Switch } from "@base-ui-components/react/switch";
 import { cn } from "@/lib/utils";
 import styles from "./switch.module.css";
 
-function SwitchRoot({
-  className,
-  ...props
-}: Switch.Root.Props) {
-  return <Switch.Root data-slot="switch-root" className={cn(styles.root, className)} {...props} />;
+function SwitchRoot({ className, ...props }: Switch.Root.Props) {
+  return (
+    <Switch.Root
+      className={cn(styles.root, className)}
+      data-slot="switch-root"
+      {...props}
+    />
+  );
 }
 
-function SwitchThumb({
-  className,
-  ...props
-}: Switch.Thumb.Props) {
-  return <Switch.Thumb data-slot="switch-thumb" className={cn(styles.thumb, className)} {...props} />;
+function SwitchThumb({ className, ...props }: Switch.Thumb.Props) {
+  return (
+    <Switch.Thumb
+      className={cn(styles.thumb, className)}
+      data-slot="switch-thumb"
+      {...props}
+    />
+  );
 }
 
 export { SwitchRoot as Switch, SwitchThumb };
