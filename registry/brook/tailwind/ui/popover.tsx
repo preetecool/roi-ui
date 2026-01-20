029770f3997f35745d1b5b6da86802ef9cef5722
preetecool <preet@creal.ca>
2026-01-19 23:21:54 -0500

    tailwind popover

--- registry/brook/tailwind/ui/popover.tsx
+++ registry/brook/tailwind/ui/popover.tsx
@@ -13,7 +13,11 @@ function PopoverRoot({ ...props }: Popover.Root.Props) {
 function PopoverTrigger({ className, ...props }: Popover.Trigger.Props) {
   return (
     <Popover.Trigger
-      className={cn("data-[popup-open]:bg-accent data-[popup-open]:text-accent-foreground", className)}
+      className={cn(
+        "data-[popup-open]:!bg-accent data-[popup-open]:!text-accent-foreground",
+        "focus-visible:outline-2 focus-visible:outline-ring focus-visible:-outline-offset-1",
+        className
+      )}
       data-slot="popover-trigger"
       nativeButton
       {...props}
@@ -74,7 +78,7 @@ function PopoverPopup({
       >
         <Popover.Popup
           className={cn(
-            "relative flex h-[var(--popup-height,auto)] w-[var(--popup-width,auto)] origin-[var(--transform-origin)] rounded-[var(--radius)] bg-popover text-popover-foreground shadow-[0_0_0_0.5px_oklch(from_var(--border)_l_c_h_/_0.8),var(--shadow-lg)] transition-[width,height,transform,opacity] duration-250 ease-[var(--ease-out-expo)] will-change-[transform,opacity] data-[ending-style]:scale-95 data-[starting-style]:scale-95 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0",
+            "relative flex h-[var(--popup-height,auto)] w-[var(--popup-width,auto)] origin-[var(--transform-origin)] rounded-[var(--radius)] bg-popover bg-clip-padding text-popover-foreground shadow-[0_0_0_0.5px_oklch(from_var(--border)_l_c_h_/_0.8),var(--shadow-lg)] transition-[width,height,transform,opacity] duration-250 ease-[var(--ease-out-expo)] will-change-[transform,_opacity] data-[ending-style]:scale-95 data-[starting-style]:scale-95 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0",
             className
           )}
           data-slot="popover-popup"
@@ -180,8 +184,8 @@ function PopoverContent({ className, style, ...props }: Popover.Popup.Props) {
       <PopoverPositioner sideOffset={8}>
         <Popover.Popup
           className={cn(
-            "!relative box-border origin-[var(--transform-origin)] rounded-[var(--radius)] bg-popover px-4 py-2 text-popover-foreground",
-            "transition-[transform,opacity] duration-250 ease-[var(--ease-out-expo)] will-change-[transform,opacity]",
+            "!relative box-border origin-[var(--transform-origin)] rounded-[var(--radius)] bg-popover bg-clip-padding px-4 py-2 text-popover-foreground",
+            "transition-[transform,opacity] duration-250 ease-[var(--ease-out-expo)] will-change-[transform,_opacity]",
             "shadow-[0_0_0_0.5px_oklch(from_var(--border)_l_c_h_/_0.8),var(--shadow-lg)]",
             "z-[1000]",
             "data-[starting-style]:scale-95 data-[starting-style]:opacity-0",
